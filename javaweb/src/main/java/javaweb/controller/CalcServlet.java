37b381e705cc001640be32290a66eda8bf8ea64d
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-02 15:00:27 +0800

    web

--- javaweb/src/main/java/javaweb/controller/CalcServlet.java
+++ javaweb/src/main/java/javaweb/controller/CalcServlet.java
@@ -23,12 +23,23 @@ public class CalcServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// UTF-8 編碼
+		req.setCharacterEncoding("UTF-8");  // 請求編碼
+		resp.setCharacterEncoding("UTF-8"); // 回應編碼
+		resp.setContentType("text/plain;charset=UTF-8"); // 文件編碼
+		
 		// 透過 req.getParameter("x") 取得 x 與 req.getParameter("y") 取得 y 的參數
 		String x = req.getParameter("x");
 		String y = req.getParameter("y");
 		
 		resp.getWriter().println("x = " + x);
 		resp.getWriter().println("y = " + y);
+		
+		// 判斷 x, y 是否有輸入 ?
+		if(x == null || y == null) {
+			resp.getWriter().println("請輸入 x, y 參數");
+			return;
+		}
 		
 		String pathInfo = req.getPathInfo();
 		
