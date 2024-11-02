343b31f71ce624fd3606a177fc93fa50ad6d7ec2
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-02 16:10:35 +0800

    web

--- javaweb/src/main/java/javaweb/controller/ExchangeServlet2.java
+++ javaweb/src/main/java/javaweb/controller/ExchangeServlet2.java
@@ -1,6 +1,10 @@
 package javaweb.controller;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -21,7 +25,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/exchange2")
 public class ExchangeServlet2 extends HttpServlet {
-
+	int change = 0;
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// UTF-8 編碼
@@ -34,13 +38,26 @@ public class ExchangeServlet2 extends HttpServlet {
 			resp.getWriter().println("請輸入零錢 change=?");
 			return;
 		}
-		int change = Integer.parseInt(changeStr); // 字串轉 int
+		
+		change = Integer.parseInt(changeStr); // 字串轉 int
 		resp.getWriter().println("找零錢 " + change + " 元");
 		
 		// ----------------------------------------------------------
 		
+		// 定義面額
+		List<Integer> denoms = List.of(50,  10,  5, 1);
 		
+		// {50=1, 10=3, 5=1, 1=2}
+		Map<Integer, Integer> result = denoms.stream().collect(
+					Collectors.toMap(denom -> denom, 
+									 denom -> {
+										 int count = change / denom;
+										 change = change % denom;
+										 return count;
+									 })
+				);
 		
+		resp.getWriter().println(result);
 		
 	}
 	
