334f710fb5ac5d1b50d678cad2e2f160e1f3d6b5
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-02 15:31:37 +0800

    web

---
+++ javaweb/src/main/java/javaweb/controller/ExchangeServlet.java
@@ -0,0 +1,34 @@
+package javaweb.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/** 
+ * 找零錢程式
+ * 使用者輸入零錢 change=87
+ * 網址: /exchange?change=87
+ * 輸出: 50元 1 個
+ * 		 10元 3 個
+ * 		  5元 1 個
+ * 		  1元 2 個
+ */
+
+@WebServlet("/exchange")
+public class ExchangeServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String changeStr = req.getParameter("change");
+		if(changeStr == null) {
+			resp.getWriter().println("請輸入零錢 change=?");
+			return;
+		}
+		
+	}
+	
+}
