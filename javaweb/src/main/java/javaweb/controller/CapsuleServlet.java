81642738bc02ec41bec02d7a2144dea93a6be7cb
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-09 15:47:30 +0800

    web

--- javaweb/src/main/java/javaweb/controller/CapsuleServlet.java
+++ javaweb/src/main/java/javaweb/controller/CapsuleServlet.java
@@ -1,6 +1,9 @@
 package javaweb.controller;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.Stream;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -18,8 +21,30 @@ public class CapsuleServlet extends HttpServlet {
 			{"5", "獅薩", "5"}
 	};
 	
+	private Random random = new Random();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		// 開始抽獎
+		// 獎號
+		String myId = String.valueOf(random.nextInt(awards.length) + 1);  
+		// 獎項
+		String[] myAward = Stream.of(awards)
+							.filter(award->award[0].equals(myId))
+							.findAny()
+							.orElseGet(null);
+		// 獎項名字
+		String myAwardName = myAward[1];
+		// 獎項庫存
+		int myAwardQty = Integer.parseInt(myAward[2]);
+		
+		System.out.println("獎號 myId: " + myId);
+		System.out.println("獎項 myAward: " + Arrays.toString(myAward));
+		System.out.println("獎項名字 myAwardName: " + myAward[1]);
+		System.out.println("獎項庫存 myAwardQty: " + myAward[2]);
+		
+		
 		req.setAttribute("awards", awards);
 		// 指向到 jsp
 		req.getRequestDispatcher("/WEB-INF/view/capsule.jsp").forward(req, resp);
