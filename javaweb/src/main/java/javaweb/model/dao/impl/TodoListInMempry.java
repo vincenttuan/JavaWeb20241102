9a90473e44d02b358406940a803befaed7a78204
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-16 11:44:30 +0800

    web

--- javaweb/src/main/java/javaweb/model/dao/impl/TodoListInMempry.java
+++ javaweb/src/main/java/javaweb/model/dao/impl/TodoListInMempry.java
@@ -1,5 +1,40 @@
 package javaweb.model.dao.impl;
 
-public class TodoListInMempry {
+import java.util.List;
+
+import javaweb.model.dao.TodoListDao;
+import javaweb.model.entity.Todo;
+
+public class TodoListInMempry implements TodoListDao {
+
+	@Override
+	public List<Todo> findAllTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Todo getTodo(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addTodo(Todo todo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTodoComplete(Integer id, Boolean completed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteTodo(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
