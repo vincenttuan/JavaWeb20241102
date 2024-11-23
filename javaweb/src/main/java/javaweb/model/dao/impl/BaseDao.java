0a1f0f01173e8f9c78875f2b841f589a60ba8b0e
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-23 11:02:45 +0800

    web

--- javaweb/src/main/java/javaweb/model/dao/impl/BaseDao.java
+++ javaweb/src/main/java/javaweb/model/dao/impl/BaseDao.java
@@ -1,16 +1,24 @@
 package javaweb.model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 // 用來讓不同的 xxxDao 透過 extends BaseDao 可以連接 MySQL
 public class BaseDao {
 	protected static Connection conn;
 	
 	static {
+		// 連線參數
 		String username = "root";
 		String password = "12345678";
 		String dbUrl = "jdbc:mysql://localhost:3306/web?serverTimezone=Asia/Taipei&characterEncoding=utf-8&useUnicode=true&useSSL=false";
-		
+		// 建立連線
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); // 註冊 MySQL Driver
+			conn = DriverManager.getConnection(dbUrl, username, password); // 連線建立
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
