f5bb43dc3976bd4f48a9fc4e7b63e39a8c193c4a
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-23 16:16:34 +0800

    web

---
+++ javaweb/src/main/java/javaweb/cart/controller/EmailConfirmServlet.java
@@ -0,0 +1,24 @@
+package javaweb.cart.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/cart/email/confirm")
+public class EmailConfirmServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;chatset=utf-8");
+		
+		String id = req.getParameter("id");
+		resp.getWriter().print("<h1>User id = " + id + " email 驗證成功</h1>");
+		
+	}
+	
+}
