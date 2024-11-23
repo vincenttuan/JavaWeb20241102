bc5e65845b10be7967e86bfd1db2bf7811ee49e4
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-23 16:08:58 +0800

    web

--- javaweb/src/main/java/javaweb/cart/service/EmailService.java
+++ javaweb/src/main/java/javaweb/cart/service/EmailService.java
@@ -13,7 +13,7 @@ public class EmailService {
 	// Google應用程式密碼
 	// 請參考此篇 https://www.yongxin-design.com/Article/10
 	// 請自行產生Google應用程式密碼
-	String googleAppPassword = "elre qnee sbiq uqwj";
+	String googleAppPassword = "";
 
 	// 寄件者的電子郵件地址
 	String from = "vincenttuan@gmail.com";
