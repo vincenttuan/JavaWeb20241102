8a247648b73f72fd0e0c5ceafd1ff5fd7bddbb83
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-30 12:00:25 +0800

    web

--- javaweb/src/main/java/javaweb/cart/dao/UserRegisterDAO.java
+++ javaweb/src/main/java/javaweb/cart/dao/UserRegisterDAO.java
@@ -1,5 +1,24 @@
 package javaweb.cart.dao;
 
-public interface UserRegisterDAO {
+import java.util.List;
 
+import org.apache.catalina.User;
+
+// 定義對 user 資料表進行 CRUD 的功能
+public interface UserRegisterDAO {
+	// 查詢所有 user
+	List<User> findAllUsers();
+	
+	// 查詢指定 user (根據 id 來找到該名 user)
+	User getUserById(Integer id);
+	
+	// 查詢指定 user (根據 username 來找到該名 user)
+	User getUserByName(String username);
+		
+	// 新增 user
+	int addUser(User user);
+	
+	// 修改 password
+	int updatePasswordById(Integer id, String newPassword);
+	
 }
