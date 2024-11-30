a88fa1f8ac2fe640e9225435ee4760a5d2bf339c
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-30 16:11:42 +0800

    web

--- javaweb/src/main/java/javaweb/cart/service/impl/UserListServiceImpl.java
+++ javaweb/src/main/java/javaweb/cart/service/impl/UserListServiceImpl.java
@@ -2,14 +2,19 @@ package javaweb.cart.service.impl;
 
 import java.util.List;
 
+import javaweb.cart.dao.UserListDAO;
+import javaweb.cart.dao.impl.UserListDaoImpl;
 import javaweb.cart.model.dto.UserDTO;
 import javaweb.cart.service.UserListService;
 
 public class UserListServiceImpl implements UserListService {
-
+	private UserListDAO userListDAO = new UserListDaoImpl();
 	@Override
 	public List<UserDTO> findAllUsers() {
-		// TODO Auto-generated method stub
+		// 1.從 userListDAO 取得 List<User>
+		
+		// 2.將 List<User> 每一個元素由 User 轉成 UserDTO 使之變成 List<UserDTO>
+		
 		return null;
 	}
 	
