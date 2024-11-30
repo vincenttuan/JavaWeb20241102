64f86bd84e60d3e2a70a32f4b4653122909be1d0
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-30 16:17:40 +0800

    web

--- javaweb/src/main/java/javaweb/cart/service/impl/UserListServiceImpl.java
+++ javaweb/src/main/java/javaweb/cart/service/impl/UserListServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javaweb.cart.dao.UserListDAO;
 import javaweb.cart.dao.impl.UserListDaoImpl;
 import javaweb.cart.model.dto.UserDTO;
+import javaweb.cart.model.entity.User;
 import javaweb.cart.service.UserListService;
 
 public class UserListServiceImpl implements UserListService {
@@ -12,10 +13,21 @@ public class UserListServiceImpl implements UserListService {
 	@Override
 	public List<UserDTO> findAllUsers() {
 		// 1.從 userListDAO 取得 List<User>
+		List<User> users = userListDAO.findAllUsers();
 		
 		// 2.將 List<User> 每一個元素由 User 轉成 UserDTO 使之變成 List<UserDTO>
+		List<UserDTO> userDTOs = users.stream()
+									  .map(user -> {
+										  UserDTO userDTO = new UserDTO();
+										  userDTO.setId(user.getId());
+										  userDTO.setUsername(user.getUsername());
+										  userDTO.setEmail(user.getEmail());
+										  userDTO.setCompleted(user.getCompleted());
+										  return userDTO;
+									  })
+									  .toList();
 		
-		return null;
+		return userDTOs;
 	}
 	
 }
