610f66fe9958c72db31b984b77aac5e42235e480
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-30 10:15:30 +0800

    web

--- javaweb/src/main/java/javaweb/cart/controller/UserRegisterServlet.java
+++ javaweb/src/main/java/javaweb/cart/controller/UserRegisterServlet.java
@@ -14,7 +14,9 @@ public class UserRegisterServlet extends HttpServlet {
 	// 透過網址進行 GET 請求要做的事
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		// RequestDispatcher 分派器
+		// forward // 向前行 GO~~~
+		req.getRequestDispatcher("/WEB-INF/view/cart/user_register.jsp").forward(req, resp);
 	}
 	
 	// 透過表單進行 POST 請求要做的事
