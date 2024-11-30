07214cfcabaf030c75083cdc51170ad5baf86126
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-30 10:05:51 +0800

    web

---
+++ javaweb/src/main/java/javaweb/cart/controller/UserRegisterServlet.java
@@ -0,0 +1,26 @@
+package javaweb.cart.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/user/register")
+public class UserRegisterServlet extends HttpServlet {
+	
+	// 透過網址進行 GET 請求要做的事
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+	// 透過表單進行 POST 請求要做的事
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	}
+	
+}
