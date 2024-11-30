91fba7eff121885dd5e1718cf496426dd18cb84b
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-30 15:46:03 +0800

    web

---
+++ javaweb/src/main/java/javaweb/cart/dao/impl/UserListDaoImpl.java
@@ -0,0 +1,16 @@
+package javaweb.cart.dao.impl;
+
+import java.util.List;
+
+import javaweb.cart.dao.UserListDAO;
+import javaweb.cart.model.entity.User;
+
+public class UserListDaoImpl implements UserListDAO {
+
+	@Override
+	public List<User> findAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
