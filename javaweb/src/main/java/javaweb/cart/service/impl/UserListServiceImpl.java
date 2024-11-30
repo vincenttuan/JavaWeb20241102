0e3b0f9bbef31f78f5dfcfe9c6a66f41a8ad7292
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-30 16:09:31 +0800

    web

---
+++ javaweb/src/main/java/javaweb/cart/service/impl/UserListServiceImpl.java
@@ -0,0 +1,16 @@
+package javaweb.cart.service.impl;
+
+import java.util.List;
+
+import javaweb.cart.model.dto.UserDTO;
+import javaweb.cart.service.UserListService;
+
+public class UserListServiceImpl implements UserListService {
+
+	@Override
+	public List<UserDTO> findAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
