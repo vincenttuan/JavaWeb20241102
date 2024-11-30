49109b1574dd497c27248a62690e004c01733b30
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-30 14:17:44 +0800

    web

--- javaweb/src/main/java/javaweb/cart/controller/UserRegisterServlet.java
+++ javaweb/src/main/java/javaweb/cart/controller/UserRegisterServlet.java
@@ -29,16 +29,17 @@ public class UserRegisterServlet extends HttpServlet {
 	// 透過表單進行 POST 請求要做的事
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// 接收到表單傳來的資料
+		// 1. 接收到表單傳來的資料
 		String username = req.getParameter("username");
 		String password = req.getParameter("password");
 		String email = req.getParameter("email");
-		// 將資料傳給 service 進行新增程序
+		
+		// 2. 將資料傳給 service 進行新增資料程序(資料庫新增一筆資料)
 		userRegisterService.addUser(username, password, email);
 		resp.getWriter().println("Add OK");
 		resp.getWriter().flush();
 		
-		// 發送 email 給新註冊的用戶
+		// 3. 發送 email 給新註冊的用戶(發送確認信給新註冊的用戶)
 		emailService.sendEmail(email, "http://localhost:8080/javaweb/cart/email/confirm");
 		resp.getWriter().println("Email send OK");
 		
