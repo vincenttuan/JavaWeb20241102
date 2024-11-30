2e4a5976d1c0fca11d51d6873dc481d7d913b599
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-11-30 15:02:30 +0800

    web

--- javaweb/src/main/java/javaweb/cart/controller/EmailConfirmServlet.java
+++ javaweb/src/main/java/javaweb/cart/controller/EmailConfirmServlet.java
@@ -7,16 +7,24 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import javaweb.cart.service.UserRegisterService;
+import javaweb.cart.service.impl.UserRegisterServiceImpl;
 
 @WebServlet("/cart/email/confirm")
 public class EmailConfirmServlet extends HttpServlet {
+	
+	private UserRegisterService userRegisterService = new UserRegisterServiceImpl();
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setCharacterEncoding("UTF-8");
 		resp.setContentType("text/html;chatset=utf-8");
 		
+		// 得到 username
 		String username = req.getParameter("username");
+		
+		// email 驗證成功
+		userRegisterService.emailConfirmOK(username);
 		
 		resp.getWriter().print("<h1>User name = " + username + " email 驗證成功</h1>");
 		
