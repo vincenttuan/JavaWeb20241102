c27d6b214b421fe2d76a566a32f9da1c7f2a82e6
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-07 12:02:18 +0800

    web

--- javaweb/src/main/java/javaweb/cart/dao/impl/UserLoginDAOImpl.java
+++ javaweb/src/main/java/javaweb/cart/dao/impl/UserLoginDAOImpl.java
@@ -15,19 +15,20 @@ public class UserLoginDAOImpl extends BaseDAO implements UserLoginDAO {
 		try(PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			pstmt.setString(1, username);
 			// 執行查詢
-			ResultSet rs = pstmt.executeQuery();
-			// 判斷是否有資料
-			if(rs.next()) {
-				// 建立 User 物件並注入欄位資料
-				User user = new User();
-				user.setId(rs.getInt("id"));
-				user.setUsername(rs.getString("username"));
-				user.setHashPassword(rs.getString("hash_password"));
-				user.setHashSalt(rs.getString("hash_salt"));
-				user.setEmail(rs.getString("email"));
-				user.setCompleted(rs.getBoolean("completed"));
-				
-				return user;
+			try(ResultSet rs = pstmt.executeQuery()) {
+				// 判斷是否有資料
+				if(rs.next()) {
+					// 建立 User 物件並注入欄位資料
+					User user = new User();
+					user.setId(rs.getInt("id"));
+					user.setUsername(rs.getString("username"));
+					user.setHashPassword(rs.getString("hash_password"));
+					user.setHashSalt(rs.getString("hash_salt"));
+					user.setEmail(rs.getString("email"));
+					user.setCompleted(rs.getBoolean("completed"));
+					
+					return user;
+				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
