6925e54b8dea051621a146be60527cf0be6dbcf2
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-07 10:11:21 +0800

    web

--- javaweb/src/main/java/javaweb/cart/controller/UserRegisterServlet.java
+++ javaweb/src/main/java/javaweb/cart/controller/UserRegisterServlet.java
@@ -36,13 +36,24 @@ public class UserRegisterServlet extends HttpServlet {
 		
 		// 2. 將資料傳給 service 進行新增資料程序(資料庫新增一筆資料)
 		userRegisterService.addUser(username, password, email);
-		resp.getWriter().println("Add OK");
-		resp.getWriter().flush();
+		//resp.getWriter().println("Add OK");
+		//resp.getWriter().flush();
 		
 		// 3. 發送 email 給新註冊的用戶(發送確認信給新註冊的用戶)
 		emailService.sendEmail(email, "http://localhost:8080/javaweb/cart/email/confirm?username=" + username);
-		resp.getWriter().println("Email send OK");
+		//resp.getWriter().println("Email send OK");
 		
+		// 準備要傳遞給 result.jsp 的資訊
+		String resultTitle = "註冊結果";
+		String resultMessage = "用戶 " + username + " 註冊成功 !";
+		resultMessage += "<p />";
+		resultMessage += "系統已送出驗證信件到 " + email + " 信箱, 請收信並點選[確認]連結";
+		
+		req.setAttribute("resultTitle", resultTitle);
+		req.setAttribute("resultMessage", resultMessage);
+		
+		// 重導到 result.jsp
+		req.getRequestDispatcher("/WEB-INF/view/cart/result.jsp").forward(req, resp);
 	}
 	
 }
