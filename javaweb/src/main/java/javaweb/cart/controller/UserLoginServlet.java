41f5b7974689d8c49c6a577df428570fef3c5046
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-07 12:29:33 +0800

    web

--- javaweb/src/main/java/javaweb/cart/controller/UserLoginServlet.java
+++ javaweb/src/main/java/javaweb/cart/controller/UserLoginServlet.java
@@ -7,10 +7,14 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import javaweb.cart.service.UserLoginService;
+import javaweb.cart.service.impl.UserLoginServiceImpl;
 
 @WebServlet("/user/login")
 public class UserLoginServlet extends HttpServlet {
-
+	
+	private UserLoginService userLoginService = new UserLoginServiceImpl();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.getRequestDispatcher("/WEB-INF/view/cart/login.jsp").forward(req, resp);
@@ -23,6 +27,23 @@ public class UserLoginServlet extends HttpServlet {
 		String authcode = req.getParameter("authcode");
 		
 		// 驗證帳號與密碼
+		String resultTitle = "登入結果";
+		String resultMessage = "";
+		try {
+			boolean checkLogin = userLoginService.login(username, password); // 進行驗證並取得結果
+			if(checkLogin) { // 驗證成功
+				resultMessage = username + " 登入成功";
+			}
+		} catch (RuntimeException e) { // 驗證失敗
+			resultMessage = e.getMessage(); // 失敗原因
+		}
+		
+		// 給 result.jsp 的資訊
+		req.setAttribute("resultTitle", resultTitle);
+		req.setAttribute("resultMessage", resultMessage);
+		
+		// 重導到 result.jsp
+		req.getRequestDispatcher("/WEB-INF/view/cart/result.jsp").forward(req, resp);
 		
 	}
 	
