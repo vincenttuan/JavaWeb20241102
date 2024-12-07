0f8c46dc557ab590e7c7a0fba2f983bcde298efb
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-07 10:25:38 +0800

    web

---
+++ javaweb/src/main/java/javaweb/cart/dao/impl/BaseDAO.java
@@ -0,0 +1,25 @@
+package javaweb.cart.dao.impl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class BaseDAO {
+	static Connection conn; // 資料庫連線
+	
+	static {
+		try {
+			// 1. 註冊 MySQL Driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			// 2. 建立連線
+			String username = "root"; // MySQL 連線帳號
+			String password = "12345678"; // MySQL 連線密碼
+			String dbUrl = "jdbc:mysql://localhost:3306/web?serverTimezone=Asia/Taipei&useSSL=false"; // MySQL 連線位置
+			conn = DriverManager.getConnection(dbUrl, username, password);
+					
+		} catch (Exception e) {
+			e.printStackTrace(); // 若有錯誤則將錯誤訊息印出
+		}
+		
+	}
+}
--- javaweb/src/main/java/javaweb/cart/dao/impl/UserListDaoImpl.java
+++ javaweb/src/main/java/javaweb/cart/dao/impl/UserListDaoImpl.java
@@ -1,7 +1,5 @@
 package javaweb.cart.dao.impl;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,26 +9,7 @@ import java.util.List;
 import javaweb.cart.dao.UserListDAO;
 import javaweb.cart.model.entity.User;
 
-public class UserListDaoImpl implements UserListDAO {
-	
-	static Connection conn; // 資料庫連線
-	
-	static {
-		try {
-			// 1. 註冊 MySQL Driver
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			// 2. 建立連線
-			String username = "root"; // MySQL 連線帳號
-			String password = "12345678"; // MySQL 連線密碼
-			String dbUrl = "jdbc:mysql://localhost:3306/web?serverTimezone=Asia/Taipei&useSSL=false"; // MySQL 連線位置
-			conn = DriverManager.getConnection(dbUrl, username, password);
-					
-		} catch (Exception e) {
-			e.printStackTrace(); // 若有錯誤則將錯誤訊息印出
-		}
-		
-	}
+public class UserListDaoImpl extends BaseDAO implements UserListDAO {
 	
 	// 查詢所有使用者
 	@Override
