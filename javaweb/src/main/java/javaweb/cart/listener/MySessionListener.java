b4f68005679e3ac745a1fb43715bd5652fe62165
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-07 16:33:44 +0800

    web

--- javaweb/src/main/java/javaweb/cart/listener/MySessionListener.java
+++ javaweb/src/main/java/javaweb/cart/listener/MySessionListener.java
@@ -12,25 +12,15 @@ public class MySessionListener implements HttpSessionListener {
 	// Session 創建監聽
 	@Override
 	public void sessionCreated(HttpSessionEvent se) {
-		// 監聽 username 加入到 session 中, 表示有人登入了
-		HttpSession session = se.getSession();
-		if(session.getAttribute("username") != null) {
-			String username = session.getAttribute("username") + "";
-			loginCount++;
-			System.out.println("有人登入了, username = " + username + ", 目前人數: " + loginCount);
-			
-		}
+		loginCount++;
+		System.out.println("目前人數: " + loginCount);
 	}
 	
 	// Session 失效監聽
 	@Override
 	public void sessionDestroyed(HttpSessionEvent se) {
-		HttpSession session = se.getSession();
-		if(session.getAttribute("username") != null) {
-			String username = session.getAttribute("username") + "";
-			loginCount--;
-			System.out.println("有人登出了, username = " + username + ", 目前人數: " + loginCount);
-		}
+		loginCount--;
+		System.out.println("目前人數: " + loginCount);
 	}
 	
 }
