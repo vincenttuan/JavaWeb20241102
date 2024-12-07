aaff62c7d7c00e7258308fdc947c45a05f4e285e
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-07 16:28:05 +0800

    web

--- javaweb/src/main/java/javaweb/cart/listener/MySessionListener.java
+++ javaweb/src/main/java/javaweb/cart/listener/MySessionListener.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpSessionListener;
 // 監聽 session 的創建與使用
 @WebListener
 public class MySessionListener implements HttpSessionListener {
-	
+	public static int loginCount = 0;
 	// Session 創建監聽
 	@Override
 	public void sessionCreated(HttpSessionEvent se) {
@@ -17,6 +17,7 @@ public class MySessionListener implements HttpSessionListener {
 		if(session.getAttribute("username") != null) {
 			String username = session.getAttribute("username") + "";
 			System.out.println("有人登入了, username = " + username);
+			loginCount++;
 		}
 	}
 	
@@ -27,6 +28,7 @@ public class MySessionListener implements HttpSessionListener {
 		if(session.getAttribute("username") != null) {
 			String username = session.getAttribute("username") + "";
 			System.out.println("有人登出了, username = " + username);
+			loginCount--;
 		}
 	}
 	
