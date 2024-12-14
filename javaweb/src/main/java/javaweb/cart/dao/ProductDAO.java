7d1f9958d6dda6b4637b10a3a8000363ea7c22f5
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-14 13:58:12 +0800

    web

--- javaweb/src/main/java/javaweb/cart/dao/ProductDAO.java
+++ javaweb/src/main/java/javaweb/cart/dao/ProductDAO.java
@@ -1,5 +1,14 @@
 package javaweb.cart.dao;
 
+import java.util.List;
+
+import javaweb.cart.model.entity.Product;
+
 public interface ProductDAO {
-	
+	void add(Product product); // 新增紀錄
+	void update(Integer productId, Product newProduct); // 修改紀錄, 根據指定 productId
+	void updateQty(Integer productId, Integer amount);  // 修改庫存紀錄, 根據指定 productId 與數量
+	void delete(Integer productId); // 刪除庫存, 根據指定 productId
+	List<Product> findAll(); // 全部查詢
+	Product getById(Integer productId); // 單筆查詢
 }
