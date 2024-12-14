ebb0321630c9eb993b5fc90422e498805790ee65
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-14 14:09:20 +0800

    web

--- javaweb/src/main/java/javaweb/cart/dao/impl/ProductDAOImpl.java
+++ javaweb/src/main/java/javaweb/cart/dao/impl/ProductDAOImpl.java
@@ -1,5 +1,7 @@
 package javaweb.cart.dao.impl;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import javaweb.cart.dao.ProductDAO;
@@ -10,6 +12,21 @@ public class ProductDAOImpl extends BaseDAO implements ProductDAO {
 	@Override
 	public void add(Product product) {
 		String sql = "insert into product(product_name, price, qty, image_base64) values(?, ?, ?, ?)";
+		
+		try(PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			// 配置資料到 ?
+			pstmt.setString(1, product.getProductName());
+			pstmt.setInt(2, product.getPrice());
+			pstmt.setInt(3, product.getQty());
+			pstmt.setString(4, product.getImageBase64());
+			
+			// 執行新增
+			int rowcount = pstmt.executeUpdate();
+			System.out.println("新增成功筆數:" + rowcount);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
