ca9c7ecca1620407c17be762abb9928e9f77d04e
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-14 13:47:21 +0800

    web

---
+++ javaweb/src/main/java/javaweb/cart/model/entity/Product.java
@@ -0,0 +1,12 @@
+package javaweb.cart.model.entity;
+
+// 對應 product 資料表中的每一筆紀錄
+public class Product {
+	private Integer productId; // 對應 product_id 欄位
+	private String productName; // 對應 product_name 欄位
+	private Integer price; // 對應 price 欄位
+	private Integer qty; // 對應 qty 欄位
+	private String imageBase64; // 對應 image_base64 欄位
+	
+	
+}
