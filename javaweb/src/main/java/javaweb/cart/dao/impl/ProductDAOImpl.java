b7a22c040308c64a590dae92946610f293ff5c2e
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-14 14:00:56 +0800

    web

---
+++ javaweb/src/main/java/javaweb/cart/dao/impl/ProductDAOImpl.java
@@ -0,0 +1,22 @@
+package javaweb.cart.dao.impl;
+
+import java.util.List;
+
+import javaweb.cart.dao.ProductDAO;
+import javaweb.cart.model.entity.Product;
+
+public class ProductDAOImpl extends BaseDAO implements ProductDAO {
+
+	@Override
+	public void add(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Product> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
