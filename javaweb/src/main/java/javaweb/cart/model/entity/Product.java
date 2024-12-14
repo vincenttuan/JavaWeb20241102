7a28b59f640e1befd9bc64a2999fac9872a70192
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-14 13:47:54 +0800

    web

--- javaweb/src/main/java/javaweb/cart/model/entity/Product.java
+++ javaweb/src/main/java/javaweb/cart/model/entity/Product.java
@@ -8,5 +8,36 @@ public class Product {
 	private Integer qty; // 對應 qty 欄位
 	private String imageBase64; // 對應 image_base64 欄位
 	
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public Integer getQty() {
+		return qty;
+	}
+	public void setQty(Integer qty) {
+		this.qty = qty;
+	}
+	public String getImageBase64() {
+		return imageBase64;
+	}
+	public void setImageBase64(String imageBase64) {
+		this.imageBase64 = imageBase64;
+	}
+	
 	
 }
