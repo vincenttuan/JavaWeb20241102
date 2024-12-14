57c9a00630a3691e2ef585e0a42646cca9504d56
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-14 10:31:16 +0800

    web

---
+++ javaweb/src/main/java/javaweb/cart/controller/ProductListServlet.java
@@ -0,0 +1,21 @@
+package javaweb.cart.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/product/list")
+public class ProductListServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.getRequestDispatcher("/WEB-INF/view/cart/product_list.jsp").forward(req, resp);
+		
+	}
+	
+}
