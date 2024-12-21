c99964bc528d2f2a37ba9d8f803dd5359048655f
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-21 15:44:06 +0800

    web

--- javaweb/src/main/java/javaweb/cart/service/OrderService.java
+++ javaweb/src/main/java/javaweb/cart/service/OrderService.java
@@ -2,8 +2,11 @@ package javaweb.cart.service;
 
 import java.util.List;
 
+import javaweb.cart.model.dto.OrderDTO;
 import javaweb.cart.model.dto.ProductDTO;
 
 public interface OrderService {
 	void addOrder(Integer userId, List<ProductDTO> cart); // 建立訂單(使用者id, 購物車資料)
+	
+	List<OrderDTO> findAllOrdersByUserId(Integer userId); // 查詢使用者的訂單紀錄
 }
