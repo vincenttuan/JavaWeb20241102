553296be8b1f4767eb0d8a3acf3ec6097fcfd042
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-21 10:01:19 +0800

    web

--- javaweb/src/main/java/javaweb/cart/controller/ProductOrderServlet.java
+++ javaweb/src/main/java/javaweb/cart/controller/ProductOrderServlet.java
@@ -1,18 +1,29 @@
 package javaweb.cart.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import javaweb.cart.model.dto.ProductDTO;
+import javaweb.cart.service.ProductService;
+import javaweb.cart.service.impl.ProductServiceImpl;
 
 @WebServlet("/product/order")
 public class ProductOrderServlet extends HttpServlet {
-
+	
+	private ProductService productService = new ProductServiceImpl();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 取得所有商品
+		List<ProductDTO> productDTOs = productService.findAll();
+		req.setAttribute("productDTOs", productDTOs);
+		// 重導到訂單頁面
+		req.getRequestDispatcher("/WEB-INF/view/cart/product_order_list.jsp").forward(req, resp);
 		
 	}
 
