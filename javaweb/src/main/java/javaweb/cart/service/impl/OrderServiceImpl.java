fc77a838be42e754c1ed9982e88617bd3c3901b3
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-21 15:49:35 +0800

    web

--- javaweb/src/main/java/javaweb/cart/service/impl/OrderServiceImpl.java
+++ javaweb/src/main/java/javaweb/cart/service/impl/OrderServiceImpl.java
@@ -1,10 +1,14 @@
 package javaweb.cart.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javaweb.cart.dao.OrderDAO;
 import javaweb.cart.dao.impl.OrderDAOImpl;
+import javaweb.cart.model.dto.OrderDTO;
 import javaweb.cart.model.dto.ProductDTO;
+import javaweb.cart.model.entity.Order;
+import javaweb.cart.model.entity.OrderItem;
 import javaweb.cart.service.OrderService;
 
 public class OrderServiceImpl implements OrderService {
@@ -20,6 +24,27 @@ public class OrderServiceImpl implements OrderService {
 		for(ProductDTO item : cart) {
 			orderDAO.addOrderItem(orderId, item.getProductId(), quantity);
 		}
+	}
+
+	@Override
+	public List<OrderDTO> findAllOrdersByUserId(Integer userId) {
+		List<OrderDTO> orderDTOs = new ArrayList<>();
+		// 取得訂單主檔紀錄
+		List<Order> orders = orderDAO.findAllOrdersByUserId(userId);
+		for(Order order : orders) {
+			OrderDTO orderDTO = new OrderDTO();
+			// 訂單資訊
+			orderDTO.setOrderId(order.getOrderId());
+			orderDTO.setUserId(order.getUserId());
+			// 訂單明細
+			for(OrderItem item : orderDAO.findAllOrderItemsByOrderId(order.getOrderId())) {
+				orderDTO.getItems().add(item);
+			}
+			// 注入
+			orderDTOs.add(orderDTO);
+		}
+		
+		return orderDTOs;
 	}
 
 }
