8c4eccd20d82eeff942ad1a64543d4f85bda8d89
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-21 14:53:18 +0800

    web

--- javaweb/src/main/java/javaweb/cart/model/entity/OrderItem.java
+++ javaweb/src/main/java/javaweb/cart/model/entity/OrderItem.java
@@ -5,4 +5,32 @@ public class OrderItem {
 	private Integer orderId;
 	private Integer productId;
 	private Integer quantity;
+	
+	public Integer getItemId() {
+		return ItemId;
+	}
+	public void setItemId(Integer itemId) {
+		ItemId = itemId;
+	}
+	public Integer getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	
 }
