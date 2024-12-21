a4c18188befbb6526262de386dcf04d0a4a11682
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-21 12:00:23 +0800

    web

---
+++ javaweb/src/main/java/javaweb/cart/controller/ProductCartDeleteServlet.java
@@ -0,0 +1,30 @@
+package javaweb.cart.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import javaweb.cart.model.dto.ProductDTO;
+
+// 刪除購物車內容
+@WebServlet("/product/cart/delete")
+public class ProductCartDeleteServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int index = Integer.parseInt(req.getParameter("index"));
+		
+		HttpSession session = req.getSession();
+		List<ProductDTO> cart = (List<ProductDTO>)session.getAttribute("cart");
+		cart.remove(index); // 移除
+		
+		// 重導到 cart.jsp 頁面 
+		req.getRequestDispatcher("/WEB-INF/view/cart/cart.jsp").forward(req, resp);
+	}
+	
+}
