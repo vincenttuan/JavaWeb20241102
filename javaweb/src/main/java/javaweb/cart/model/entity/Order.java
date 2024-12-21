844d39f05921f707e6361b729af5d7ada5f7ed15
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-21 14:51:31 +0800

    web

---
+++ javaweb/src/main/java/javaweb/cart/model/entity/Order.java
@@ -0,0 +1,30 @@
+package javaweb.cart.model.entity;
+
+import java.util.Date;
+
+public class Order {
+	private Integer orderId;
+	private Integer userId;
+	private Date orderDate;
+	
+	public Integer getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public Date getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	
+	
+}
---
+++ javaweb/src/main/java/javaweb/cart/model/entity/OrderItem.java
@@ -0,0 +1,5 @@
+package javaweb.cart.model.entity;
+
+public class OrderItem {
+
+}
