e7e08c6cee3ae03f22a78a4fecb0cdf786a68d61
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-21 14:32:25 +0800

    web

--- javaweb/src/main/java/javaweb/cart/controller/ProductCartSubmitServlet.java
+++ javaweb/src/main/java/javaweb/cart/controller/ProductCartSubmitServlet.java
@@ -1,12 +1,15 @@
 package javaweb.cart.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import javaweb.cart.model.dto.ProductDTO;
 import javaweb.cart.service.OrderService;
 import javaweb.cart.service.impl.OrderServiceImpl;
 
@@ -19,7 +22,16 @@ public class ProductCartSubmitServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 從 session 中取得 userId 與 cart 資訊
+		HttpSession session = req.getSession();
+		Integer userId = (Integer)session.getAttribute("userId");
+		List<ProductDTO> cart = (List<ProductDTO>)session.getAttribute("cart");
 		
+		orderService.addOrder(userId, cart);
+		
+		// 重導到 result.jsp 頁面
+		req.setAttribute("resultTitle", "購物車-結帳");
+		req.setAttribute("resultMessage", "購物車結帳完畢");
+		req.getRequestDispatcher("/WEB-INF/view/cart/result.jsp").forward(req, resp);
 		
 	}
 	
