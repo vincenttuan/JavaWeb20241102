dcd13975f714d9dc63b4c6de8e6b1838dab8d2c2
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-21 16:14:19 +0800

    web

--- javaweb/src/main/java/javaweb/cart/controller/ProductOrderHistory.java
+++ javaweb/src/main/java/javaweb/cart/controller/ProductOrderHistory.java
@@ -11,12 +11,15 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import javaweb.cart.model.dto.OrderDTO;
 import javaweb.cart.service.OrderService;
+import javaweb.cart.service.ProductService;
 import javaweb.cart.service.impl.OrderServiceImpl;
+import javaweb.cart.service.impl.ProductServiceImpl;
 
 @WebServlet("/product/order/history")
 public class ProductOrderHistory extends HttpServlet {
 	
 	private OrderService orderService = new OrderServiceImpl();
+	private ProductService productService = new ProductServiceImpl();
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -24,7 +27,9 @@ public class ProductOrderHistory extends HttpServlet {
 		Integer userId = (Integer)session.getAttribute("userId");
 		// 得到使用者歷史訂單紀錄
 		List<OrderDTO> orderDTOs = orderService.findAllOrdersByUserId(userId);
+		
 		req.setAttribute("orderDTOs", orderDTOs);
+		req.setAttribute("productDTOs", productService.findAll()); // 所有商品資訊
 		
 		req.getRequestDispatcher("/WEB-INF/view/cart/history.jsp").forward(req, resp);
 		
