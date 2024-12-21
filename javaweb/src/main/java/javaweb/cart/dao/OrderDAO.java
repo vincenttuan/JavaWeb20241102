ffa86d4a5236725b89875e5682b47078318079d4
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-21 15:02:32 +0800

    web

--- javaweb/src/main/java/javaweb/cart/dao/OrderDAO.java
+++ javaweb/src/main/java/javaweb/cart/dao/OrderDAO.java
@@ -1,7 +1,14 @@
 package javaweb.cart.dao;
 
+import java.util.List;
+
+import javaweb.cart.model.entity.Order;
+import javaweb.cart.model.entity.OrderItem;
+
 public interface OrderDAO {
 	Integer addOrder(Integer userId); // 建立訂單(主檔), 回傳訂單編號
 	void addOrderItem(Integer orderId, Integer productId, Integer quantity); // 建立訂單項目(明細檔)
 	
+	List<Order> findAllOrdersByUserId(Integer userId); // 查詢使用者的訂單
+	List<OrderItem> findAllOrderItemsByOrderId(Integer orderId); // 查詢 orderId 的明細
 }
