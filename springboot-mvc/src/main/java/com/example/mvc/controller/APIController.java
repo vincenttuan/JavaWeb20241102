d4df5365e7f9dd47cfd27c73b77b9816aa26c32e
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-28 11:58:57 +0800

    web

---
+++ springboot-mvc/src/main/java/com/example/mvc/controller/APIController.java
@@ -0,0 +1,15 @@
+package com.example.mvc.controller;
+
+/*
+ * 介紹URL所帶入的各種參數與路徑
+ * 範例:
+ *     GET  /api/query?name=John&age=25 查詢參數範例 Query Parameters 
+ *     GET  /api/path/John/25           路徑參數範例 Path Variables
+ *     POST /api/form                   表單參數範例 Form Parameters
+ *              表單資料: name=John&age=25
+ *     POST /api/json                   JSON Body 範例 可將 Json 資料注入到指定物件中(例如: User)
+ *              Json資料: {"name": "John", "age": 25}
+*/
+public class APIController {
+	
+}
