7857341804d10ae178cda4e0c857bc5911307e1b
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-28 11:18:20 +0800

    web

---
+++ springboot-mvc/src/main/java/com/example/mvc/controller/HelloMessageController.java
@@ -0,0 +1,17 @@
+package com.example.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller // 告知 Spring 本程式是一個 Controller
+public class HelloMessageController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello_message"; // 指的是 thymeleaf 的檔名
+	}
+	
+	
+}
