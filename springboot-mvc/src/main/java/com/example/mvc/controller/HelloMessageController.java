ba6a1245bfdec9eae48a7ffc20ee977058975166
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2024-12-28 11:21:06 +0800

    web

--- springboot-mvc/src/main/java/com/example/mvc/controller/HelloMessageController.java
+++ springboot-mvc/src/main/java/com/example/mvc/controller/HelloMessageController.java
@@ -3,8 +3,6 @@ package com.example.mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller // 告知 Spring 本程式是一個 Controller
 public class HelloMessageController {
