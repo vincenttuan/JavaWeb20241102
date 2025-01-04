a4d679c2588f2780d7ee0c4e532a80e94b0278a7
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-04 13:58:24 +0800

    web

--- springboot-mvc/src/main/java/com/example/mvc/repository/ResumeRepositoryImpl.java
+++ springboot-mvc/src/main/java/com/example/mvc/repository/ResumeRepositoryImpl.java
@@ -2,10 +2,18 @@ package com.example.mvc.repository;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 import com.example.mvc.model.dto.Resume;
 
+@Repository
 public class ResumeRepositoryImpl implements ResumeRepository {
-
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	@Override
 	public List<Resume> findAllResumes() {
 		// TODO Auto-generated method stub
