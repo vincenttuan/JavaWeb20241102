0037b170e8fd55fb07d1db18bda117304c989201
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-04 15:48:09 +0800

    web

--- springboot-mvc/src/main/java/com/example/mvc/repository/ResumeRepository.java
+++ springboot-mvc/src/main/java/com/example/mvc/repository/ResumeRepository.java
@@ -7,4 +7,9 @@ import com.example.mvc.model.dto.Resume;
 public interface ResumeRepository {
 	List<Resume> findAllResumes();
 	void addResume(Resume resume);
+	
+	Resume getResumeById(Integer id);
+	void updateResume(Integer id, Resume resume);
+	
+	void deleteResume(Integer id);
 }
