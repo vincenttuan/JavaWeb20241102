e8685f65421d196572cb52ba7e089e782064b6f1
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-04 13:48:15 +0800

    web

--- springboot-mvc/src/main/java/com/example/mvc/repository/ResumeRepository.java
+++ springboot-mvc/src/main/java/com/example/mvc/repository/ResumeRepository.java
@@ -1,5 +1,10 @@
 package com.example.mvc.repository;
 
+import java.util.List;
+
+import com.example.mvc.model.dto.Resume;
+
 public interface ResumeRepository {
-	
+	List<Resume> findAllResumes();
+	void addResume(Resume resume);
 }
