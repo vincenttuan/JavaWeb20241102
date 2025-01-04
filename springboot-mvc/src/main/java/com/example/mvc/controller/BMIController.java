743eaa535fbc13810f70a21a957cd163df76f628
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-04 10:40:19 +0800

    web

--- springboot-mvc/src/main/java/com/example/mvc/controller/BMIController.java
+++ springboot-mvc/src/main/java/com/example/mvc/controller/BMIController.java
@@ -3,13 +3,20 @@ package com.example.mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BMIController {
 	
-	// 網址範例:localhost:8080/bmi?h=170&w=60
+	// 顯示 bmi 表單
 	@GetMapping("/bmi")
+	public String showBMIForm() {
+		return "bmi_form";
+	}
+	
+	// 計算 bmi
+	@PostMapping("/bmi")
 	public String getBMI(Model model, @RequestParam double h, @RequestParam double w) {
 		// 計算 bmi
 		double bmi = w / Math.pow(h/100, 2);
