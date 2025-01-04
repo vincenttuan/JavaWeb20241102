2b8d2b1457e491f54e6b27b066124364cad3b79d
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-04 10:52:11 +0800

    web

---
+++ springboot-mvc/src/main/java/com/example/mvc/model/dto/BMI.java
@@ -0,0 +1,36 @@
+package com.example.mvc.model.dto;
+
+public class BMI {
+	private Double h;
+	private Double w;
+	private Double bmi;
+	private String result;
+	
+	public Double getH() {
+		return h;
+	}
+	public void setH(Double h) {
+		this.h = h;
+	}
+	public Double getW() {
+		return w;
+	}
+	public void setW(Double w) {
+		this.w = w;
+	}
+	public Double getBmi() {
+		return bmi;
+	}
+	public void setBmi(Double bmi) {
+		this.bmi = bmi;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+	
+	
+}
