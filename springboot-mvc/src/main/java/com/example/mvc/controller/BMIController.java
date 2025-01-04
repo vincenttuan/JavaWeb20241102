abe6181cc749cb1e309d8c2bdf5f4e94a96a2f70
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-04 10:27:11 +0800

    web

--- springboot-mvc/src/main/java/com/example/mvc/controller/BMIController.java
+++ springboot-mvc/src/main/java/com/example/mvc/controller/BMIController.java
@@ -13,10 +13,19 @@ public class BMIController {
 	public String getBMI(Model model, @RequestParam double h, @RequestParam double w) {
 		// 計算 bmi
 		double bmi = w / Math.pow(h/100, 2);
+		// 診斷 
+		String result = "正常";
+		if(bmi > 23) {
+			result = "過重";
+		} else if(bmi <= 18) {
+			result = "過輕";
+		}
+		
 		// 將資料放到 model 中目的是傳給 bmi.html 呈現
 		model.addAttribute("h", h);
 		model.addAttribute("w", w);
 		model.addAttribute("bmi", bmi);
+		model.addAttribute("result", result);
 		return "bmi"; // 指的是 src/main/resources/templates/bmi.html
 	}
 	
