452031fa9e8172ce5ccc3a29123e256c9a8bc6b3
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-04 14:04:14 +0800

    web

--- springboot-mvc/src/main/java/com/example/mvc/repository/ResumeRepositoryImpl.java
+++ springboot-mvc/src/main/java/com/example/mvc/repository/ResumeRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.example.mvc.repository;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -16,8 +17,8 @@ public class ResumeRepositoryImpl implements ResumeRepository {
 	
 	@Override
 	public List<Resume> findAllResumes() {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select id, name, birth, gender, education, skill, memo from resume";
+		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Resume.class));
 	}
 
 	@Override
