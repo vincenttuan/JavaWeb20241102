48f58d1316f01bea8b42f93fdf601c31e4525503
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-04 13:49:18 +0800

    web

---
+++ springboot-mvc/src/main/java/com/example/mvc/repository/ResumeRepositoryImpl.java
@@ -0,0 +1,21 @@
+package com.example.mvc.repository;
+
+import java.util.List;
+
+import com.example.mvc.model.dto.Resume;
+
+public class ResumeRepositoryImpl implements ResumeRepository {
+
+	@Override
+	public List<Resume> findAllResumes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addResume(Resume resume) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
