43e57620872cedb9083030d1d90a84c4d59bc2a0
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-04 11:03:02 +0800

    web

--- springboot-mvc/src/main/java/com/example/mvc/model/dto/BMI.java
+++ springboot-mvc/src/main/java/com/example/mvc/model/dto/BMI.java
@@ -1,48 +1,18 @@
 package com.example.mvc.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BMI {
 	private Double h;
 	private Double w;
 	private Double bmiValue;
 	private String result;
-	
-	public BMI() {
-		
-	}
-	
-	public BMI(Double h, Double w, Double bmiValue, String result) {
-		this.h = h;
-		this.w = w;
-		this.bmiValue = bmiValue;
-		this.result = result;
-	}
-	
-	public Double getH() {
-		return h;
-	}
-	public void setH(Double h) {
-		this.h = h;
-	}
-	public Double getW() {
-		return w;
-	}
-	public void setW(Double w) {
-		this.w = w;
-	}
-	public Double getBmiValue() {
-		return bmiValue;
-	}
-	public void setBmiValue(Double bmiValue) {
-		this.bmiValue = bmiValue;
-	}
-	public String getResult() {
-		return result;
-	}
-	public void setResult(String result) {
-		this.result = result;
-	}
-	
-	
-	
 	
 }
