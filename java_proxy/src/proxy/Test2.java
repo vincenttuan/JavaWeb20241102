1a4ed9989d12bbe5068a8112d57f34fccc352e7d
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-18 10:48:53 +0800

    web

--- java_proxy/src/proxy/Test2.java
+++ java_proxy/src/proxy/Test2.java
@@ -8,11 +8,18 @@ public class Test2 {
 		System.out.println("----------------");
 		Person man1 = new Man(); // 原本方式
 		man1.work();
+		Person woman1 = new Woman(); // 原本方式
+		woman1.work();
+		
 		System.out.println();
+		
 		System.out.println("有代理:");
 		System.out.println("----------------");
 		Person man2 = new PersonProxy(new Man()); // 使用代理方式
 		man2.work();
+		Person woman2 = new PersonProxy(new Woman()); // 使用代理方式
+		woman2.work();
+		
 		
 
 	}
