c8979ff38a5dc70e0fff47749a756e05cb23eb06
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-18 10:39:53 +0800

    web

--- java_proxy/src/proxy/Man.java
+++ java_proxy/src/proxy/Man.java
@@ -5,11 +5,11 @@ public class Man implements Person {
 	@Override
 	public void work() {
 		// 公用邏輯
-		System.out.println("戴口罩");
+		//System.out.println("戴口罩");
 		// 業務邏輯
 		System.out.println("上班工作");
 		// 公用邏輯
-		System.out.println("脫口罩");
+		//System.out.println("脫口罩");
 	}
 
 }
--- java_proxy/src/proxy/Woman.java
+++ java_proxy/src/proxy/Woman.java
@@ -5,11 +5,11 @@ public class Woman implements Person {
 	@Override
 	public void work() {
 		// 公用邏輯
-		System.out.println("戴口罩");
+		//System.out.println("戴口罩");
 		// 業務邏輯
 		System.out.println("市場買菜");
 		// 公用邏輯
-		System.out.println("脫口罩");
+		//System.out.println("脫口罩");
 	}
 
 }
