1ad7c5d7d130f2e2a1f2c2b25a00e65f71ac1dc8
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-18 14:00:28 +0800

    web

---
+++ java_proxy/src/aop/CalculatorImpl.java
@@ -0,0 +1,25 @@
+package aop;
+
+public class CalculatorImpl implements Calculator {
+
+	@Override
+	public int add(int x, int y) {
+		return x + y;
+	}
+
+	@Override
+	public int sub(int x, int y) {
+		return x - y;
+	}
+
+	@Override
+	public int mul(int x, int y) {
+		return x * y;
+	}
+
+	@Override
+	public int div(int x, int y) {
+		return x / y;
+	}
+	
+}
