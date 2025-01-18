4658366acb4e9930b2d8b96d7f6b012c9053779a
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-18 16:15:50 +0800

    web

--- java_proxy/src/aop/BMIImpl.java
+++ java_proxy/src/aop/BMIImpl.java
@@ -1,10 +1,11 @@
 package aop;
 
 public class BMIImpl implements BMI {
-
+	public static int count = 0; 
 	@Override
 	public double calc(double h, double w) {
-		return w / Math.pow(h/100, 2);
+		double bmi = w / Math.pow(h/100, 2);
+		return bmi;
 	}
 
 }
