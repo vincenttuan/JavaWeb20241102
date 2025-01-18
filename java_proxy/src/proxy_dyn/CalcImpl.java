b818ffa9929e2406dc9e0549b851f2341cb26587
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-18 11:08:44 +0800

    web

--- java_proxy/src/proxy_dyn/CalcImpl.java
+++ java_proxy/src/proxy_dyn/CalcImpl.java
@@ -4,6 +4,8 @@ public class CalcImpl implements Calc {
 
 	@Override
 	public int add(int x, int y) {
+		// 公用邏輯(觀察 x, y 的值);
+		System.out.printf("x: %d, y:%d%n", x, y);
 		// 業務邏輯
 		int result = x + y;
 		return result;
@@ -11,6 +13,8 @@ public class CalcImpl implements Calc {
 
 	@Override
 	public int div(int x, int y) {
+		// 公用邏輯(觀察 x, y 的值);
+		System.out.printf("x: %d, y:%d%n", x, y);
 		// 業務邏輯
 		int result = x / y;
 		return result;
--- java_proxy/src/proxy_dyn/Test1.java
+++ java_proxy/src/proxy_dyn/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		Calc calc = new CalcImpl();
 		System.out.println(calc.add(20, 10));
-		System.out.println(calc.div(20, 10));
+		System.out.println(calc.div(55, 5));
 		
 	}
 }
