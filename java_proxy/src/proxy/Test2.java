31fe2804f3afd3c5bfae0826e904d8ef08ec48a2
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-18 10:45:02 +0800

    web

--- java_proxy/src/proxy/Test2.java
+++ java_proxy/src/proxy/Test2.java
@@ -4,9 +4,15 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// 利用靜態代理來實現
-		//Person man = new Man(); // 原本方式
-		Person man = new PersonProxy(new Man()); // 使用代理方式
-		
+		System.out.println("沒代理:");
+		System.out.println("----------------");
+		Person man1 = new Man(); // 原本方式
+		man1.work();
+		System.out.println();
+		System.out.println("有代理:");
+		System.out.println("----------------");
+		Person man2 = new PersonProxy(new Man()); // 使用代理方式
+		man2.work();
 		
 
 	}
