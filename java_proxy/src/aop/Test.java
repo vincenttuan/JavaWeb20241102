c4a322152769f161aad92419b5681103a479a247
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-18 14:40:30 +0800

    web

--- java_proxy/src/aop/Test.java
+++ java_proxy/src/aop/Test.java
@@ -8,11 +8,19 @@ public class Test {
 		AnnotationConfigApplicationContext context = 
 				new AnnotationConfigApplicationContext(AppConfig.class);
 		// 取得 Calculator 物件(Bean)
+		System.out.println("Calculator:");
 		Calculator calculator = context.getBean(Calculator.class);
 		System.out.println(calculator.add(30, 10));
 		System.out.println(calculator.sub(30, 10));
 		System.out.println(calculator.mul(30, 10));
 		System.out.println(calculator.div(30, 10));
+		
+		System.out.println();
+		
+		System.out.println("BMI:");
+		// 取得 BMI 物件(Bean)
+		BMI bmi = context.getBean(BMI.class);
+		System.out.println(bmi.calc(170, 60));
 		
 	}
 }
