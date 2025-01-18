254bb873e1e3dbdbcbd91de67a56542390d11619
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-18 12:19:17 +0800

    web

--- java_proxy/src/proxy_dyn/MyPrintAspect.java
+++ java_proxy/src/proxy_dyn/MyPrintAspect.java
@@ -7,14 +7,14 @@ import java.util.Arrays;
 // 紀錄方法前後所要做的事
 public class MyPrintAspect {
 	
-	// 代理執行任務(公用邏輯)
+	// 前置通知 (Advice)
 	public static void before(Method method, Object[] args) {
 		// 代理執行任務(公用邏輯)
 		System.out.printf("方法呼叫前-方法啟動: %s 參數: %s\n", method.getName(), Arrays.toString(args));
 		
 	}
 	
-	// 代理執行任務(公用邏輯)
+	// 後置通知 (Advice)
 	public static void end() {
 		// 代理執行任務(公用邏輯)
 		System.out.println("方法呼叫後-方法結束");
