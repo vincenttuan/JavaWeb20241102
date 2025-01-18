48bb4423586acebd86628082fecd6c6a3228a2eb
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-18 10:23:45 +0800

    w

---
+++ java_proxy/src/proxy/Test1.java
@@ -0,0 +1,12 @@
+package proxy;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		Person man = new Man();
+		Person woman = new Woman();
+		man.work();
+		woman.work();
+	}
+
+}
