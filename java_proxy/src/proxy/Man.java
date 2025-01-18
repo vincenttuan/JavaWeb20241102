2634e2495552d35f350837610a74f363c8abdaba
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-18 10:32:32 +0800

    web

--- java_proxy/src/proxy/Man.java
+++ java_proxy/src/proxy/Man.java
@@ -4,8 +4,12 @@ public class Man implements Person {
 
 	@Override
 	public void work() {
+		// 公用邏輯
+		System.out.println("戴口罩");
 		// 業務邏輯
 		System.out.println("上班工作");
+		// 公用邏輯
+		System.out.println("脫口罩");
 	}
 
 }
