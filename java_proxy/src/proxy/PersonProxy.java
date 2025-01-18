255524e24b943236322fcc2429ab03b58a78826c
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-18 10:38:45 +0800

    web

---
+++ java_proxy/src/proxy/PersonProxy.java
@@ -0,0 +1,24 @@
+package proxy;
+
+// 靜態代理
+// 代理執行任務
+public class PersonProxy implements Person {
+	// 代理對象
+	private Person person;
+	
+	// 透過建構子可以將  Man 或 Woman 物件傳入
+	public PersonProxy(Person person) {
+		this.person = person;
+	}
+
+	@Override
+	public void work() {
+		// 代理執行任務(公用邏輯)
+		System.out.println("戴口罩");
+		// 代理執行任務(業務邏輯)
+		person.work();
+		// 代理執行任務(公用邏輯)
+		System.out.println("脫口罩");
+	}
+	
+}
