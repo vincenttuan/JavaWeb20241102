8e675510a386e438beb542a35ff50e9edbbc7b99
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-01-18 11:58:02 +0800

    web

--- java_proxy/src/proxy_dyn/CalcImpl.java
+++ java_proxy/src/proxy_dyn/CalcImpl.java
@@ -5,7 +5,7 @@ public class CalcImpl implements Calc {
 	@Override
 	public int add(int x, int y) {
 		// 公用邏輯(觀察 x, y 的值);
-		System.out.printf("觀察 x: %d, y:%d%n", x, y);
+		//System.out.printf("觀察 x: %d, y:%d%n", x, y);
 		// 業務邏輯
 		int result = x + y;
 		return result;
@@ -14,7 +14,7 @@ public class CalcImpl implements Calc {
 	@Override
 	public int div(int x, int y) {
 		// 公用邏輯(觀察 x, y 的值);
-		System.out.printf("觀察 x: %d, y:%d%n", x, y);
+		//System.out.printf("觀察 x: %d, y:%d%n", x, y);
 		// 業務邏輯
 		int result = x / y;
 		return result;
