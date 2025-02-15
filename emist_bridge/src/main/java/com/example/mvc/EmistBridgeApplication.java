63ec027cfb4dd1cfeb1e0628f204f1af2cce7232
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-02-15 10:39:27 +0800

    web

---
+++ emist_bridge/src/main/java/com/example/mvc/EmistBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmistBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmistBridgeApplication.class, args);
+	}
+
+}
