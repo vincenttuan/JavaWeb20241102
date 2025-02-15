3d4b84a54a3a33b82c56c72722a63d46b5f0e3c3
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-02-15 11:41:25 +0800

    web

--- emist_order/src/main/java/com/example/mvc/controller/RegisterController.java
+++ emist_order/src/main/java/com/example/mvc/controller/RegisterController.java
@@ -10,10 +10,23 @@ public class RegisterController {
 	
 	@GetMapping("/register")
 	public String register(HttpServletRequest request) {
+		String portocol = request.getScheme(); // 取得通訊協定 http or https
 		String ip = request.getLocalAddr(); // 取得本機端 ip
 		int port = request.getLocalPort(); // 取得本機端 port
-		String message = "註冊: " + ip + ":" + port;
+		String url = portocol + "://" + ip + ":" + port;
+		String message = "註冊: " + url;
 		return message;
 	}
+	
+	@GetMapping("/unregister")
+	public String unregister(HttpServletRequest request) {
+		String portocol = request.getScheme(); // 取得通訊協定 http or https
+		String ip = request.getLocalAddr(); // 取得本機端 ip
+		int port = request.getLocalPort(); // 取得本機端 port
+		String url = portocol + "://" + ip + ":" + port;
+		String message = "取消註冊: " + url;
+		return message;
+	}
+	
 	
 }
