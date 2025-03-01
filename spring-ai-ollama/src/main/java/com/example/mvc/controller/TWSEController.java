53ac12b33aae3b39b6c6ab567d59dd11397a0981
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-01 14:43:28 +0800

    web

--- spring-ai-ollama/src/main/java/com/example/mvc/controller/TWSEController.java
+++ spring-ai-ollama/src/main/java/com/example/mvc/controller/TWSEController.java
@@ -7,12 +7,14 @@ import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 public class TWSEController {
 	
 	@GetMapping("/twse")
