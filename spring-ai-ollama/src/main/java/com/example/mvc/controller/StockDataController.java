1be1008fc10dda0759add52c786ea1034e14cd52
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-08 14:49:12 +0800

    web

--- spring-ai-ollama/src/main/java/com/example/mvc/controller/StockDataController.java
+++ spring-ai-ollama/src/main/java/com/example/mvc/controller/StockDataController.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ import com.google.gson.Gson;
 
 @RestController
 @RequestMapping("/api/stock")
+@CrossOrigin(origins = "http://localhost:8080")
 public class StockDataController {
 	
 	@Autowired
