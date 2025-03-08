071aa4449ab0a1ac72e44dcb3e580271b9de6009
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-08 10:19:44 +0800

    web

---
+++ spring-ai-ollama/src/main/resources/mysql.sql
@@ -0,0 +1,13 @@
+-- 建立 stock_data 資料表
+create table stock_data (
+	id bigint auto_increment primary key,
+    date date not null, -- 日期欄位 'yyyy-MM-dd' 格式
+    symbol varchar(10), -- 證券代號 
+	name varchar(10), -- 證券名稱
+    price decimal(10, 2), -- 收盤價
+    yield decimal(5, 2), -- 殖利率(%)
+    year int, -- 股利年度
+    pe decimal(5, 2), -- 本益比 (pe: price to earnings ratio)
+    pb decimal(5, 2), -- 股價淨值比 (pb: price to book ratio)
+    period varchar(10) -- 財報年/季
+)
