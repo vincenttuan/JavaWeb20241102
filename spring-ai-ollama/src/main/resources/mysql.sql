aa394f60de890e9e127ba691a8629abfe10cdcea
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-08 12:42:59 +0800

    web

--- spring-ai-ollama/src/main/resources/mysql.sql
+++ spring-ai-ollama/src/main/resources/mysql.sql
@@ -5,9 +5,9 @@ create table stock_data (
     symbol varchar(10), -- 證券代號 
 	name varchar(10), -- 證券名稱
     price decimal(10, 2), -- 收盤價
-    yield decimal(5, 2), -- 殖利率(%)
+    yield decimal(10, 2), -- 殖利率(%)
     year int, -- 股利年度
-    pe decimal(5, 2), -- 本益比 (pe: price to earnings ratio)
-    pb decimal(5, 2), -- 股價淨值比 (pb: price to book ratio)
+    pe decimal(10, 2), -- 本益比 (pe: price to earnings ratio)
+    pb decimal(10, 2), -- 股價淨值比 (pb: price to book ratio)
     period varchar(10) -- 財報年/季
 )
