afd657734d7c94b04f41b78c38e578203f286df6
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-15 12:13:55 +0800

    tx

--- springboot-tx/src/main/java/com/example/tx/service/impl/BookServiceImpl.java
+++ springboot-tx/src/main/java/com/example/tx/service/impl/BookServiceImpl.java
@@ -1,40 +1,65 @@
 package com.example.tx.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.tx.repository.BookInventoryRepository;
+import com.example.tx.repository.BookRepository;
+import com.example.tx.repository.WalletRepository;
 import com.example.tx.service.BookService;
 
 @Service
 public class BookServiceImpl implements BookService {
-
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@Autowired
+	private BookInventoryRepository bookInventoryRepository;
+	
+	@Autowired
+	private WalletRepository walletRepository;
+	
+	// 取得書本價格
 	@Override
 	public Integer getBookPrice(Integer bookId) {
-		// TODO Auto-generated method stub
-		return null;
+		return bookRepository.getBookPrice(bookId);
 	}
-
+	
+	// 取得書本庫存
 	@Override
 	public Integer getBookAmount(Integer bookId) {
-		// TODO Auto-generated method stub
-		return null;
+		return bookInventoryRepository.getBookAmount(bookId);
 	}
-
+	
+	// 取得帳戶餘額
 	@Override
 	public Integer getWalletBalance(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		return walletRepository.getWalletBalance(username);
 	}
-
+	
+	// 更新書本庫存
 	@Override
 	public void reduceBookAmount(Integer bookId, Integer amountToReduce) {
-		// TODO Auto-generated method stub
-		
+		// 1. 取得目前庫存
+		Integer bookAmount = getBookAmount(bookId);
+		// 2. 若庫存不足(目前庫存量 < amountToReduce)則拋出例外
+		if(bookAmount < amountToReduce) {
+			throw new RuntimeException("庫存不足 " + bookAmount + " < " + amountToReduce);
+		}
+		// 3. 更新庫存
+		bookInventoryRepository.updateBookAmount(amountToReduce, bookId);
 	}
 
 	@Override
 	public void updateWalletBalance(String username, Integer bookPrice) {
-		// TODO Auto-generated method stub
-		
+		// 1. 取得目前餘額
+		Integer walletBalance = getWalletBalance(username);
+		// 2. 若餘額不足(目前餘額 < bookPrice)則拋出例外
+		if(walletBalance < bookPrice) {
+			throw new RuntimeException("餘額不足 " + walletBalance + " < " + bookPrice);
+		}
+		// 3. 更新餘額
+		walletRepository.updateWalletBalance(bookPrice, username);
 	}
 
 }
