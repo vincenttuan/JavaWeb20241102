e1638913d008b0556311ec592a9b83ddc37ff467
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-15 11:05:13 +0800

    tx

---
+++ springboot-tx/src/main/java/com/example/tx/entity/BookInventoty.java
@@ -0,0 +1,22 @@
+package com.example.tx.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity // 自動生成資料表
+public class BookInventoty {
+	@Id
+	private Integer bookId; // 書號
+	@Column
+	private Integer bookAmount; // 庫存數量
+	
+}
