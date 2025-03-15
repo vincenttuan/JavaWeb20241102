e4bdb7d159c0be650961814692e069d07d72fd5b
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-15 10:50:49 +0800

    tx

--- springboot-tx/src/main/java/com/example/tx/entity/Book.java
+++ springboot-tx/src/main/java/com/example/tx/entity/Book.java
@@ -1,5 +1,8 @@
 package com.example.tx.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,12 +12,15 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity // 自動生成資料表
 public class Book {
-	
+	@Id
 	private Integer bookId; // 書號
 	
+	@Column
 	private String bookName; // 書名
 	
+	@Column
 	private Integer bookPrice; // 價格
 	
 }
