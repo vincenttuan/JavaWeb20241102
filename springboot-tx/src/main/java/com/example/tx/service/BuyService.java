7f61b9127a0c274db862e6e428a9271e3603ab4e
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-15 15:40:02 +0800

    tx

--- springboot-tx/src/main/java/com/example/tx/service/BuyService.java
+++ springboot-tx/src/main/java/com/example/tx/service/BuyService.java
@@ -1,6 +1,8 @@
 package com.example.tx.service;
 
+import com.example.tx.exception.InsufficientAmount;
+
 // 買書服務
 public interface BuyService {
-	void buyOneBook(String username, Integer bookId);
+	void buyOneBook(String username, Integer bookId) throws InsufficientAmount;
 }
