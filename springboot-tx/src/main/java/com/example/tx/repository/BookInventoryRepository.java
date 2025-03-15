4000adb83807249b86ab48bdb076a75015fa3257
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-15 11:55:24 +0800

    tx

--- springboot-tx/src/main/java/com/example/tx/repository/BookInventoryRepository.java
+++ springboot-tx/src/main/java/com/example/tx/repository/BookInventoryRepository.java
@@ -1,10 +1,13 @@
 package com.example.tx.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.tx.entity.BookInventory;
+
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface BookInventoryRepository extends JpaRepository<BookInventory, Integer> {
@@ -14,6 +17,8 @@ public interface BookInventoryRepository extends JpaRepository<BookInventory, In
 	Integer getBookAmount(Integer bookId);
 	
 	// 更新庫存數量(目前存量 - amountToReduce)
+	@Modifying
+	@Transactional
 	@Query(value = "update book_inventory set book_amount = book_amount - :amountToReduce where book_id = :bookId", nativeQuery = true)
 	void updateBookAmount(Integer amountToReduce, Integer bookId);
 	
