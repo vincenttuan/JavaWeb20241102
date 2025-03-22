b15bf4f7debb64bdcb63a4b21224c93e8e8666f4
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-22 11:54:49 +0800

    j

--- react/my-react-study/src/App3.jsx
+++ react/my-react-study/src/App3.jsx
@@ -3,13 +3,15 @@ import './App.css'
 
 function App() {
     const text = 'Hello React + Springboot'
-
+    const imgSrc = 'https://pic.pimg.tw/imksw/1661562296-228137591-g_n.jpg'
     return (
         <>
-            <h1>{text}</h1>
+            <h1 style={{backgroundColor: 'red'}}>{text}</h1>
             <h1>{text} {text}</h1>
             <h1>{text.toUpperCase()}</h1>
             <input type="text" value={text} />
+            <p />
+            <img src={imgSrc}></img>
         </>
     )
 
