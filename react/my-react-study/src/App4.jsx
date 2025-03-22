040bb6b7c1a6d7b9b4bdfe779ad72bdabbc4481e
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-22 12:07:48 +0800

    j

--- react/my-react-study/src/App4.jsx
+++ react/my-react-study/src/App4.jsx
@@ -1,9 +1,27 @@
 // 事件處理
+
 function App() {
+
+    const handleClick1 = function() {
+        alert('OK1');
+    };
+
+    const handleClick2 = () => {
+        alert('OK2');
+    };
+
+    const handleClick3 = (e) => {
+        // e: 事件資訊
+        alert('OK3 ' + e);
+        console.log(e);
+    };
 
     return(
         <>
-            <button onClick={function(){alert('OK1');}}>我是按鈕 1</button><p />
+            <button onClick={function(){alert('OK');}}>我是按鈕</button><p />
+            <button onClick={handleClick1}>我是按鈕 1</button><p />
+            <button onClick={handleClick2}>我是按鈕 2</button><p />
+            <button onClick={handleClick3}>我是按鈕 3</button><p />
         </>
     )
 
