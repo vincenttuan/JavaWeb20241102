b90db1236cebfb3486b4dff08dcf95a51fbe44e5
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-22 12:17:40 +0800

    j

---
+++ react/my-react-study/src/App5.jsx
@@ -0,0 +1,23 @@
+// 陣列
+
+function App() {
+    // 建立陣列
+    const fruits = ['Apple', 'Orange', 'Banana', 'Watermelon']
+
+    // 利用 <li> 標籤將陣列內容條列出來
+    // key 是幫助 React 來識別每一個元素得唯一性
+    const listFruits = [
+        <li key="1">Apple</li>,
+        <li key="2">Orange</li>,
+        <li key="3">Banana</li>,
+        <li key="4">Watermelon</li>
+    ]
+    return (
+        <>
+            {fruits}
+            {listFruits}
+        </>
+    )    
+}
+
+export default App
--- react/my-react-study/src/main.jsx
+++ react/my-react-study/src/main.jsx
@@ -5,11 +5,12 @@ import App from './App.jsx'
 import App2 from './App2.jsx'
 import App3 from './App3.jsx'
 import App4 from './App4.jsx'
+import App5 from './App5.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <App4 />
+    <App5 />
   </>
   
 )
