a6d160eaf27e09aa3b2dec7451a7b0b145d4d97c
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-22 15:39:30 +0800

    j

--- springboot-mvc/src/main/java/com/example/mvc/controller/SendMailController.java
+++ springboot-mvc/src/main/java/com/example/mvc/controller/SendMailController.java
@@ -1,6 +1,7 @@
 package com.example.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.mvc.SendMail;
 
 @RestController
+//@CrossOrigin(origins = "http://localhost:5173") // 允許 http://localhost:5173
+@CrossOrigin(origins = "*") // 允許來自任何地方的請求
 public class SendMailController {
 	
 	@PostMapping("/sendmail")
