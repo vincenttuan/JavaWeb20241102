62407b70a56c358d3fc1039621fec5771722dbd9
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-22 16:08:43 +0800

    a

---
+++ react/my-react-study/src/App9.jsx
@@ -0,0 +1,19 @@
+// 輸入框狀態
+import React, {useState} from "react";
+
+function App() {
+    //const [name, setName] = React.useState('')
+    const [name, setName] = useState('')
+
+    return (
+        <div>
+            名字: <input type="text" value="name" onChange={(e) => setName(e.target.value)} />
+            <p />
+            所輸入的名字是: {name}
+            <p />
+        </div>
+    )
+
+}
+
+export default App
