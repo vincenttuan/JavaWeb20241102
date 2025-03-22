b809f666e68a43919103b73cba1ec672c281905e
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-22 11:58:39 +0800

    j

---
+++ react/my-react-study/src/App4.jsx
@@ -0,0 +1,13 @@
+// 事件處理
+function App() {
+
+    return(
+        <>
+            <button onClick={function(){alert('OK1');}}>我是按鈕 1</button><p />
+        </>
+    )
+
+}
+
+export default App
+
--- react/my-react-study/src/main.jsx
+++ react/my-react-study/src/main.jsx
@@ -4,11 +4,12 @@ import Hello from './Hello.jsx'
 import App from './App.jsx'
 import App2 from './App2.jsx'
 import App3 from './App3.jsx'
+import App4 from './App4.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <App3 />
+    <App4 />
   </>
   
 )
