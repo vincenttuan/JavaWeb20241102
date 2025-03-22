19d46403b43bea3893aa80e1a964a1edd27f3437
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-22 13:44:58 +0800

    j

---
+++ react/my-react-study/src/App6.jsx
@@ -0,0 +1,30 @@
+// 陣列 map, filter 的使用
+
+function App() {
+    // 陣列
+    const fruits = [
+        {id:1, name:'Apple', price:20},
+        {id:2, name:'Banana', price:30},
+        {id:3, name:'Orange', price:40}
+    ]
+
+    // 陣列過濾
+    const filterFruits = fruits.filter((fruit) => {
+        return fruit.price > 25;
+    })
+
+    return (
+        <>
+            <h2>顯示所有水果</h2>
+            <ul>
+                {fruits.map((fruit, index) => (
+                    <li key={index}>{fruit.name} ${fruit.price}</li>
+                ))}
+            </ul>
+            <hr />
+
+        </>
+    )
+}
+
+export default App
--- react/my-react-study/src/main.jsx
+++ react/my-react-study/src/main.jsx
@@ -6,11 +6,12 @@ import App2 from './App2.jsx'
 import App3 from './App3.jsx'
 import App4 from './App4.jsx'
 import App5 from './App5.jsx'
+import App6 from './App6.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <App5 />
+    <App6 />
   </>
   
 )
