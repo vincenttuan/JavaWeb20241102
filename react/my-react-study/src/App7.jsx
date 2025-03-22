012e6cdac3596dbfda65c794cd0298704c6cb4db
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-22 15:42:29 +0800

    j

---
+++ react/my-react-study/src/App7.jsx
@@ -0,0 +1,12 @@
+import SendMailForm from "./SendMailForm";
+
+function App() {
+    return(
+        <>
+            <h1>Email Sending Application</h1>
+            <SendMailForm />
+        </>        
+    )
+}
+
+export default App;
--- react/my-react-study/src/main.jsx
+++ react/my-react-study/src/main.jsx
@@ -7,11 +7,12 @@ import App3 from './App3.jsx'
 import App4 from './App4.jsx'
 import App5 from './App5.jsx'
 import App6 from './App6.jsx'
+import App7 from './App7.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <App6 />
+    <App7 />
   </>
   
 )
