14c717c746feaa706ca4f1f369a14d619d0af0f5
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-29 15:42:06 +0800

    web

---
+++ react/my-react-study/src/App12.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+function App() {
+    const [guess, setGuess] = useState('');
+    const [result, setResult] = useState('');
+
+    const handleSubmit = async() => {
+        const url = `http://localhost:8080/game/guess?num=${guess}`;
+        const response = await fetch(url); // 傳送請求
+        const result = await response.text(); // 取得回應
+        setResult(result);
+    }
+
+    return (
+        <div>
+            <h1>多人猜數字遊戲</h1>
+            <input type="number" maxLength="4" 
+                value={guess} onChange={(e) => setGuess(e.target.value)} />
+            <button onClick={handleSubmit}>提交測試</button>    
+            <h2>遊戲結果</h2>
+            <ul>
+                {result}
+            </ul>
+        </div>
+    )
+}
+
+export default App;
