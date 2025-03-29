31f5d567735d86880970bc2906e80c8365a4b6bd
MB-teacher <MB-teacher@DESKTOP-D075LVA>
2025-03-29 16:23:05 +0800

    web

--- react/my-react-study/src/App12.jsx
+++ react/my-react-study/src/App12.jsx
@@ -22,12 +22,21 @@ function App() {
         setResult(result);
     }
 
+    const handleRestart = async() => {
+        const url = 'http://localhost:8080/game/restart';
+        const response = await fetch(url); // 傳送請求
+        const result = await response.text(); // 取得回應
+        setResult(result);
+    }
+
     return (
         <div>
             <h1>多人猜數字遊戲</h1>
             <input type="number" maxLength="4" 
                 value={guess} onChange={(e) => setGuess(e.target.value)} />
             <button onClick={handleSubmit}>提交測試</button>    
+            <p />
+            <button onClick={handleRestart}>新局開啟</button>    
             <h2>遊戲結果</h2>
             <h2>
                 <pre>
